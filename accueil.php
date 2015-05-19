49710b8812d225473cefae18f31f07c2f44a61d8
TomKfr <tkieffer67@gmail.com>
2015-05-19 13:43:39 +0200

    Antécédents médicaux fonctionnels

--- accueil.php
+++ accueil.php
@@ -7,6 +7,10 @@ require "./src/bootstrap.php";
 require_once './model/user.php';
 require_once './model/dossier.php';
 
+function echo_erreur(){
+    echo '<p>Login et/ou mot de passe incorrect !</p>';
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,70 +22,72 @@ require_once './model/dossier.php';
     
     <?php
 
-    $login = htmlspecialchars($_POST['login']);
-    $pwd = htmlspecialchars($_POST['pwd']);
-		// if (isset($login) AND $login ==  "medecin" AND isset($pwd) AND $pwd ==  "medecin") // Si login et le mot de passe sont OK
-  //   {
-    // On affiche la page
-    	if (isset($login) && $login ==  "medecin"){
+    if(isset($_POST['login'])) $login = htmlspecialchars($_POST['login']);
+    if(isset($_POST['pwd'])) $pwd = htmlspecialchars($_POST['pwd']);
+
+    if(isset($login)){
+    	if ($login ==  "medecin"){
     		$user = $entityManager->getRepository('User')->find($login);
     		if(!is_null($user)){
     			if(password_verify($pwd, $user->password)){
     				$_SESSION['user'] = $user->login;
-    		
-    ?>
-        <h1>Bienvenue Médecin ! <?php echo $_SESSION['user']; ?> </h1>
-		<p> <a href="./etat_civil.php">Etat civil</a> </p>
-		<p> <a href="./coordonnees.php">Coordonnées</a> </p>
-		<p> <a href="./antecedents.php">Antécédents médicaux</a> </p>
-		<p> <a href="./vaccinations.php">Vaccinations</a> </p>
-        <p> <a href="./logout.php">Déconnexion</a> </p>
-
-		
-        <?php
     		}
-    		else echo "erreur !";
+    		else echo_erreur();
     	}
-    	else echo "utilisateur inconnu !";
+    	else echo_erreur();
     }
-	// else if (isset($login) AND $login ==  "infirmiere" AND isset($pwd) AND $pwd ==  "infirmiere") {
-    elseif (isset($login) && $login ==  "infirmiere"){
+    elseif ($login ==  "infirmiere"){
     		$user = $entityManager->getRepository('User')->find($login);
     		if(!is_null($user)){
     			if(password_verify($pwd, $user->password)){
     				$_SESSION['user'] = $user;
-	?>
-		<h1>Bienvenue Infirmière ! </h1>
-		<p> <a href="./etat_civil.php">Etat civil</a> </p>
-		<p> <a href="./coordonnees.php">Coordonnées</a> </p>
-        <p> <a href="./logout.php">Déconnexion</a> </p>
-		<?php
 			}
-		}
+            else echo_erreur();
+        }
+        else echo_erreur();
 	}
-	// else if (isset($login) AND $login ==  "employe" AND isset($pwd) AND $pwd ==  "employe") {
-	elseif (isset($login) && $login ==  "employe"){
+	elseif ($login ==  "employe"){
     		$user = $entityManager->getRepository('User')->find($login);
     		if(!is_null($user)){
     			if(password_verify($pwd, $user->password)){
     				$_SESSION['user'] = $user;
-		?>
-		<h1>Bienvenue Employé ! </h1>
-		<p> <a href="./etat_civil.php">Etat civil</a> </p>
-		<p> <a href="./coordonnees.php">Coordonnées</a> </p>
-		<p> <a href="./antecedents.php">Antécédents médicaux</a> </p>
-		<p> <a href="./vaccinations.php">Vaccinations</a> </p>
-        <p> <a href="./logout.php">Déconnexion</a> </p>
-		<?php
 			}
-		}
+            else echo_erreur();
+        }
+        else echo_erreur();
 	}
-    else // Sinon, on affiche un message d'erreur
-    {
-        echo '<p>Login et/ou mot de passe incorrect !</p>';
+}
+
+    //On affiche la vue correspondante :
+    if($_SESSION['user'] == "medecin"){
+        ?>
+        <h1>Bienvenue Médecin ! </h1>
+        <p> <a href="./etat_civil.php">Etat civil</a> </p>
+        <p> <a href="./coordonnees.php">Coordonnées</a> </p>
+        <p> <a href="./antecedents.php">Antécédents médicaux</a> </p>
+        <p> <a href="./vaccinations.php">Vaccinations</a> </p>
+        <p> <a href="./logout.php">Déconnexion</a> </p>
+        <?php
+    }
+    elseif($_SESSION['user'] == "infirmiere"){
+        ?>
+        <h1>Bienvenue Infirmière ! </h1>
+        <p> <a href="./etat_civil.php">Etat civil</a> </p>
+        <p> <a href="./coordonnees.php">Coordonnées</a> </p>
+        <p> <a href="./logout.php">Déconnexion</a> </p>
+        <?php
+    }
+    if($_SESSION['user'] == "employe"){
+        ?>
+        <h1>Bienvenue Employé ! </h1>
+        <p> <a href="./etat_civil.php">Etat civil</a> </p>
+        <p> <a href="./coordonnees.php">Coordonnées</a> </p>
+        <p> <a href="./antecedents.php">Antécédents médicaux</a> </p>
+        <p> <a href="./vaccinations.php">Vaccinations</a> </p>
+        <p> <a href="./logout.php">Déconnexion</a> </p>
+        <?php
     }
     ?>
-    
         
     </body>
 </html>
--- antecedents.php
+++ antecedents.php
@@ -17,16 +17,31 @@ require_once './model/dossier.php';
     <body>
     
     <?php
+    // echo "user : ".isset($_SESSION['user'])?$_SESSION['user']:"null";
+    // echo "sel_user : ".isset($_GET['sel_user'])?$_GET['sel_user']:"null";
+    // echo "antc : ".isset($_GET['antc'])?$_GET['antc']:"null";
+    // echo "login : ".isset($_GET['login'])?$_GET['login']:"null";
 
-    if(!isset($_SESSION['user'])){
+    if(isset($_GET['sel_user'])){ // Si on a sélectionné un utilisateur à visualier/modifier
+        $selected_user = $entityManager->getRepository('User')->find($_GET['sel_user']);
+    }
+
+    if(isset($_GET['antc'])){ // On a envoyé une modification
+        $selected_user = $entityManager->getRepository('User')->find($_GET['login']);
+        if($selected_user == null) echo "c'est null !!!!!!";
+    }
+    else
+
+    if(!isset($_SESSION['user'])){ // Si l'utilisateur n'est pas connecté
     	echo "<h1>Vous devez être connecté pour accéder à cette page !</h1>";
     }
-    else {
+    else {  // Si il est connecté
     $user = $_SESSION['user'];
     		
     ?>
-        <h1>Modification des coordonnées : </h1>
-        <form method='POST' action='./coordonnes.php'>
+        <h1>Modification des antécédants médicaux : </h1>
+        <p>Connecté en tant que : <?php echo $user ?> => <a href='./logout.php'>Déconnexion</a></p>
+        <form method='GET' action='./antecedents.php'>
         <label>Sélectionner un utilisateur :</label>
         <select name="sel_user">
         	<?php
@@ -38,8 +53,23 @@ require_once './model/dossier.php';
             </select>
         	<input type="submit" value="Valider" />
         </form> 
+        <br><br>
+        <form method="GET" action="./antecedents.php">
+            <label>Antécédents de <?php if(isset($selected_user)) echo $selected_user->login; ?> :</label>
+            <input type="text" name="antc" value=<?php if(isset($selected_user)) echo "'".$selected_user->dossier->antc."'"; if($user != 'medecin') echo "disabled"; ?> ></value>
+            <input type="hidden" name="login" value=<?php if(isset($selected_user)) echo "'".$selected_user->login."'"; ?> />
+            <?php if($user == 'medecin') echo "<input type='submit' />"; else echo "<br>Vous n'avez pas le droit de modifier les antécédants médicaux"; ?>
+        </form>
 <?php	
 }
+
+    if(isset($_GET['antc'])){ // On a envoyé une modification
+        $selected_user = $entityManager->getRepository('User')->find($_GET['login']);
+        $selected_user->dossier->antc = $_GET['antc'];
+        $entityManager->flush();
+
+        echo "<p> Mise à jour réussie ! <a href='./accueil.php'>Revenir à l'accueil</a></p>";
+    }
 ?>
     
         
